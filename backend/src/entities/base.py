2769f6a61ce790c863884f78c73e26315e76d9e5
Charbel Abi Daher <abidahercharbel@gmail.com>
2021-11-13 20:10:17 +0200

    Initialized SQLAlchemy and placeholders for entities

---
+++ backend/src/entities/base.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db_url = 'localhost:5432'
+db_name = 'competition'
+db_user = 'usr'
+db_password = 'pass'
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
