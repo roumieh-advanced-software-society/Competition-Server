b2dd724b1271b5871b5b3b7bf3c97ce37dbeaa79
Charbel Abi Daher <abidahercharbel@gmail.com>
2021-11-30 13:11:34 +0000

    Increased pool size

--- backend/src/entities/base.py
+++ backend/src/entities/base.py
@@ -8,7 +8,7 @@ db_url = 'localhost:5432'
 db_name = 'competition'
 db_user = 'usr'
 db_password = 'pass'
-engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}', pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
